b44a1bfb48bc332ca920b90b6a8e61031b3b662b
Ohgodwhydidyoutakeit <w.lipowski97@gmail.com>
2023-10-02 19:42:10 +0200

    feat(eui): components

--- fe-todo/src/ui/MainBody.component.tsx
+++ fe-todo/src/ui/MainBody.component.tsx
@@ -4,11 +4,15 @@ import { WeekDays } from "../components/domain/WeekDays.enum";
 import { Priority } from "../components/domain/Priority.enum";
 import './MainBody.component.scss'
 import { EffortLink } from "../components/effort-links/EffortLink.component";
+import { InputCard } from "../components/input-card/InputCard.component";
 
 
 export const MainBody: FC = () => {
     return <div className="container">
-        <DueDate weekday={WeekDays.FRIDAY} priority={Priority.LOW} />
-        <EffortLink priority={Priority.HIGH} />
+        <h1>
+            TODO
+        </h1>
+        <InputCard />
+
     </div>
 }
