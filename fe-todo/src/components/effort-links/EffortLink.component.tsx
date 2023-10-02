cbe3bea7311e05774a8c997889b08d9105633f79
Ohgodwhydidyoutakeit <w.lipowski97@gmail.com>
2023-10-02 15:48:36 +0200

    feat(eui): effort link component

--- fe-todo/src/components/effort-links/EffortLink.component.tsx
+++ fe-todo/src/components/effort-links/EffortLink.component.tsx
@@ -1,8 +1,16 @@
 import { FC } from "react";
+import { Priority } from "../domain/Priority.enum";
+import { ClassNameMapper } from "../utils/ClassNameMapper.util";
 
+import './EffortLink.component.scss'
 
+interface IEffortLinkProps {
+    priority: Priority
+}
 
-export const EffortLink: FC = () => {
-    return <>
-    </>
+export const EffortLink: FC<IEffortLinkProps> = (props: IEffortLinkProps) => {
+    return (
+        <span className={ClassNameMapper("eui-effort-link", props.priority)}>
+        </span>
+    )
 }
--- fe-todo/src/ui/MainBody.component.tsx
+++ fe-todo/src/ui/MainBody.component.tsx
@@ -3,10 +3,12 @@ import { DueDate } from "../components/due-date/DueDate.component";
 import { WeekDays } from "../components/domain/WeekDays.enum";
 import { Priority } from "../components/domain/Priority.enum";
 import './MainBody.component.scss'
+import { EffortLink } from "../components/effort-links/EffortLink.component";
 
 
 export const MainBody: FC = () => {
     return <div className="container">
         <DueDate weekday={WeekDays.FRIDAY} priority={Priority.LOW} />
+        <EffortLink priority={Priority.HIGH} />
     </div>
 }
