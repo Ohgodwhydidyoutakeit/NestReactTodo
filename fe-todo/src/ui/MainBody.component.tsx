e2494f4abad94001588191d1dd0db956d55e70fd
Ohgodwhydidyoutakeit <w.lipowski97@gmail.com>
2023-10-03 19:48:37 +0200

    feat(infra): i18n translations configuration

--- fe-todo/src/ui/MainBody.component.tsx
+++ fe-todo/src/ui/MainBody.component.tsx
@@ -3,14 +3,17 @@ import { DueDate } from "../components/due-date/DueDate.component";
 import { WeekDays } from "../components/domain/WeekDays.enum";
 import { Priority } from "../components/domain/Priority.enum";
 import "./MainBody.component.scss";
-import { EffortLink } from "../components/effort-links/EffortLink.component";
+
+import { t } from "i18next";
 import { InputCard } from "../components/input-card/InputCard.component";
+
 
 export const MainBody: FC = () => {
   return (
     <div className="container">
-      <DueDate weekday={WeekDays.FRIDAY} priority={Priority.LOW} />
-      <EffortLink priority={Priority.HIGH} />
+      <h1>
+        <h1>{t('header')}</h1>
+      </h1>
     </div>
   );
 };
